e4834dbac30c3ec0eae6048e79922918cc597c64
FernandaNascimento26 <nandinha.cefet@hotmail.com>
2024-09-04 14:40:22 -0300

    add CORS config

--- poc-strapi-project --quickstart/config/middlewares.js
+++ poc-strapi-project --quickstart/config/middlewares.js
@@ -1,9 +1,22 @@
 module.exports = [
-  'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'http:', 'https:', 'localhost:1337'],
+          'img-src': ["'self'", 'data:', 'blob:', 'example.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'example.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
